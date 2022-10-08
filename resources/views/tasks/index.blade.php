82bab7320d4e3cc07c0cd6632090b8862944d9f0
yayosaku1884 <ibayashi0925@gmail.com>
2022-10-08 22:18:08 +0900

    修正

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -24,7 +24,7 @@
         <div class="form-group">
             <div class="col-sm-offset-3 col-sm-6">
                 <button type="submit" class="btn btn-default">
-                    <i class="fa fa-plus"></i> 追加ボタン
+                    <i class="fa fa-plus"></i> 追加
                 </button>
             </div>
         </div>

6779bf085e11e40bdead3b19d323f8289365eb64
you13me <kurodayo0513@gmail.com>
2022-10-13 23:14:55 +0900

    コミットメッセージ

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,12 +1,32 @@
 @extends('layouts.app')
- 
+
 @section('content')
- 
+
+<div class="container-fluid">
+<div class="row justify-content-center">
+        <div class="col-md-2 px-0">
+            <div class="card">
+                <div class="card-header">{{ __('商品管理システム') }}</div>               
+                <div class="card-body2">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <a href="{{ route('items') }}">{{ __('商品一覧') }}</a>
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-md-10 px-0 ">
+            <div class="card">              
+                <div class="card-body2">
 <!-- タスク一覧表示 -->
 
 <div class="panel panel-default">
     <div class="panel-heading">
-        商品一覧画面
+        
     </div>
  <a href="/items/add">商品登録</a>
     <div class="panel-body">
@@ -37,6 +57,11 @@
                 @endforeach
             </tbody>
         </table>
+    </div>
+</div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
